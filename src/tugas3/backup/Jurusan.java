0366febf42c091d94ca2eecc609509336e489a25
poncoe <poncoe4mac@gmail.com>
2021-10-09 12:37:48 +0700

    backup tugas 3 (different code)

---
+++ src/tugas3/backup/Jurusan.java
@@ -0,0 +1,49 @@
+package tugas3.backup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jurusan {
+    private String kode;
+    private String nama;
+    private List<Mahasiswa> mhs = new ArrayList<>();
+
+    public Jurusan(String kode, String nama) {
+        this.kode = kode;
+        this.nama = nama;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void tambahMahasiswa(Mahasiswa mahasiswa) {
+        mahasiswa.tambahJurusan(this);
+        this.mhs.add(mahasiswa);
+    }
+
+    public List<Mahasiswa> getMhs() {
+        return mhs;
+    }
+
+    public void print() {
+        System.out.println("\nKode : " + this.kode);
+        System.out.println("Nama : " + this.nama);
+        System.out.println("Memiliki mahasiswa yaitu : \n");
+        for (Mahasiswa mahasiswa:this.mhs) {
+            System.out.println(mahasiswa.getNim() + " - " + mahasiswa.getNama());
+        }
+    }
+}
---
+++ src/tugas3/backup/MainTugas3.java
@@ -0,0 +1,31 @@
+package tugas3.backup;
+
+public class MainTugas3 {
+
+    public static void main(String[] args) {
+        Jurusan jurusan1 = new Jurusan("D3IF","Diploma 3 Rekayasa Perangkat Lunak Aplikasi");
+        Jurusan jurusan2 = new Jurusan("D3SI","Diploma 3 Sistem Informasi");
+
+        Mahasiswa mahasiswa1 = new Mahasiswa("6701","Rendi");
+        Mahasiswa mahasiswa2 = new Mahasiswa("6702","Chaca");
+        Mahasiswa mahasiswa3 = new Mahasiswa("6703","Agus");
+
+
+        Mahasiswa mahasiswa4 = new Mahasiswa("6301","Ridwan");
+        Mahasiswa mahasiswa5 = new Mahasiswa("6302","Siska");
+        Mahasiswa mahasiswa6 = new Mahasiswa("6303","Zayn");
+        Mahasiswa mahasiswa7 = new Mahasiswa("6304","Rahmat");
+
+        jurusan1.tambahMahasiswa(mahasiswa1);
+        jurusan1.tambahMahasiswa(mahasiswa2);
+        jurusan1.tambahMahasiswa(mahasiswa3);
+
+        jurusan2.tambahMahasiswa(mahasiswa4);
+        jurusan2.tambahMahasiswa(mahasiswa5);
+        jurusan2.tambahMahasiswa(mahasiswa6);
+        jurusan2.tambahMahasiswa(mahasiswa7);
+
+        jurusan1.print();
+        jurusan2.print();
+    }
+}
